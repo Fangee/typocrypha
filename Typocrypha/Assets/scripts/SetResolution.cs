488905c631502c97c9a7c8a8475f1dd0efd982b6
James Iwamasa <jiwamasa@ucsc.edu>
2017-12-05 17:17:39 -0800

    Fixed some resolution/quality things.
    Author: James Iwamasa.
    
    Added script to hard set resolution to 1024/568.
    Changed quality settings for pixel perfection.

---
+++ Typocrypha/Assets/scripts/SetResolution.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// sets resolution of screen
+public class SetResolution : MonoBehaviour {
+	public int width;
+	public int height;
+
+	void Start () {
+		Screen.SetResolution (width, height, false);
+	}
+}
