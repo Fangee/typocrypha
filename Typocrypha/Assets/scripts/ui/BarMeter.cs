b4f212826e5e282e9e632b76a7e993bfe0472472
James Iwamasa <jiwamasa@ucsc.edu>
2017-12-08 23:09:42 -0800

    Added health/shield bars for player.
    Author: James Iwamasa.
    
    Health and Shield for player now have a bar meter representation.
    Functionality contained in 'BarMeter.cs' and 'DisplayPlayer.cs'.
    To use elsewhere, you can for now copy the health bar object in the
    player stats ui, and repurpose appropiately.

---
+++ Typocrypha/Assets/scripts/ui/BarMeter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// bar meter for representing health/charge/shield/etc
+public class BarMeter : MonoBehaviour {
+	float percent; // percent full
+	float width; // width of max length of bar
+	float height; // height of bar
+	RectTransform bar; // bar's rect transform
+
+	void Start () {
+		percent = 1;
+		width = GetComponent<RectTransform> ().rect.width;
+		height = GetComponent<RectTransform> ().rect.height;
+		bar = GetComponentInChildren<RectTransform> ();
+	}
+
+	// set bar length
+	public void setValue(float new_percent) {
+		percent = new_percent >= 0 ? new_percent : 0; // don't go past 0
+		bar.localScale = new Vector3 (percent, 1, 1);
+	}
+
+}
--- Typocrypha/Assets/scripts/ui/DisplayPlayer.cs
+++ Typocrypha/Assets/scripts/ui/DisplayPlayer.cs
@@ -8,10 +8,14 @@ public class DisplayPlayer : MonoBehaviour {
 	public Text health_text; // display's player's health
 	public Text shield_text; // display's player's shield
 	public Text cast_text; // display's what player is casting
+	public BarMeter health_bar; // health bar
+	public BarMeter shield_bar; // shield bar
 
 	void Update () {
 		health_text.text = Player.main.Curr_hp.ToString ();
 		shield_text.text = Player.main.Shield.ToString ();
 		cast_text.text = Player.main.Last_cast;
+		health_bar.setValue ((float)Player.main.Curr_hp/Player.main.Max_hp);
+		shield_bar.setValue ((float)Player.main.Shield/Player.main.Max_shield);
 	}
 }
