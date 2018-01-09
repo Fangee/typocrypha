f51387c28b7e41662c9dc94c8de68a71df131d30
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2018-01-08 22:46:16 -0800

    Partial implementation of rich text for dialogue
    Dialogue can now be written in Unity's rich text format. Bugs occur if tags are nested, however (to be fixed).

--- Typocrypha/Assets/scripts/ui/TextScroll.cs
+++ Typocrypha/Assets/scripts/ui/TextScroll.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,11 +10,14 @@ public class TextScroll : MonoBehaviour {
 	public bool is_print; // is currently printing
 
 	string in_text; // current text being printed out
+	string out_buffer; // buffer for what will be shown
+	Stack<Pair<string, string>> tag_stack; // stores currently used tags
 	Text out_text; // where text gets outputted
 	Coroutine curr; // current printing coroutine
 
 	void Start() {
 		is_print = false;
+		tag_stack = new Stack<Pair<string, string>> ();
 	}
 
 	// start printing string to Text display
@@ -46,9 +50,34 @@ public class TextScroll : MonoBehaviour {
 	// print characters to Text object one by one
 	IEnumerator scrollText() {
 		int text_pos = 0;
+		out_buffer = "";
 		while (text_pos < in_text.Length) {
+			if (in_text [text_pos].CompareTo ('<') == 0) { // check if tag
+				if (in_text [text_pos + 1].CompareTo ('/') == 0) { // check if end tag (pop)
+					string end_tag = tag_stack.Pop().second; // remove tag from stack
+					out_buffer += end_tag;                   // place end tag directly into buffer
+					text_pos += end_tag.Length;              // move text_pos over after tag
+				} else { // otherwise, is a start tag (push)
+					Pair<string, string> tag = new Pair<string, string>();
+					int fstart_pos, fend_pos, estart_pos, eend_pos; // mark the '<' and '>' for the start and end respectively
+					// get starting tag
+					fstart_pos = text_pos;
+					fend_pos = in_text.IndexOf ('>', fstart_pos + 1);
+					tag.first = in_text.Substring (fstart_pos, fend_pos - fstart_pos + 1);
+					// get ending tag
+					estart_pos = in_text.IndexOf ('<', fend_pos + 1);
+					eend_pos = in_text.IndexOf ('>', estart_pos + 1);
+					tag.second = in_text.Substring (estart_pos, eend_pos - estart_pos + 1);
+					tag_stack.Push (tag);    // add tag to tag stack
+					text_pos = fend_pos + 1; // set new text_pos at end of start tag
+					out_buffer += tag.first; // add start tag to out_buffer (end tag is added later)
+					//Debug.Log("new tag:" + tag.first + ":" + tag.second);
+				}
+			}
+			if (text_pos >= in_text.Length) break;
 			AudioPlayer.main.playSFX (3); // play speaking sfx
-			out_text.text += in_text [text_pos++];
+			out_buffer += in_text [text_pos++];
+			out_text.text = out_buffer + tag_stack.Aggregate ("", (acc, next) => acc + next.second); // functional programming trick to build end tags
 			yield return new WaitForSeconds (delay);
 		}
 		is_print = false;
