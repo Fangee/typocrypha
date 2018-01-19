eaef28ec2882a226c544c25c9bd53d49d38c938c
Valentino Abate <vabate@UCSC.edu>
2018-01-19 03:06:29 -0800

    Added battlechat system and database
    -Added battle chat system and database, which stores a list of chat lines with associated chance values, sorted into categories
    -Supports any number of databases
    -Figured out how to directly link a text asset from Unity (instead of using a file path
    
    FILES ADDED
    
    ChatDatabase.cs, chatDatabase.xlsl, chatDatabase.txt
    
    FILES MODIFIED
    
    MainScene.unity, BattleManager.cs
    
    TODO
    
    currently player and enemies only use one catagory, which can't change, and doesnt allow for unique boss catagories. add a database ID value to the ICaster interface and the Ally, Player, and Enemy classes. Make the player's one get/set so their lines can change throughout the game

--- Typocrypha/Assets/scripts/management/BattleManager.cs
+++ Typocrypha/Assets/scripts/management/BattleManager.cs
@@ -11,6 +11,7 @@ public class BattleManager : MonoBehaviour {
     public GameObject ally_prefab; //prefab for ally object
     public DisplayAlly ally_left; // left ally UI
     public DisplayAlly ally_right; // right ally UI
+    public ChatDatabase chat; //Database containing chat lines
     public GameObject battleLogCast; //Casting log object and Associated reference to store
     public TextMesh logCastText;
     public TextMesh logCastInfo;
@@ -189,7 +190,7 @@ public class BattleManager : MonoBehaviour {
         {
             if(allyPos != -1)
             {
-                battleLog(spell.ToUpper().Replace(' ', '-'), "ALLY", "TODO: say something here", Utility.String.FirstLetterToUpperCase(s.root));
+                battleLog(spell.ToUpper().Replace(' ', '-'), "ALLY", chat.getLine(player_arr[allyPos].Stats.name), Utility.String.FirstLetterToUpperCase(s.root));
                 targetPattern = spellDict.getTargetPattern(s, enemy_arr, target_ind, player_arr, allyPos);
             }
             else
@@ -199,8 +200,12 @@ public class BattleManager : MonoBehaviour {
         }          
         else if (status == CastStatus.SUCCESS)
         {
-            battleLog(spell.ToUpper().Replace(' ', '-'), "PLAYER", "TODO: say something here", player_arr[player_ind].Stats.name);
+            battleLog(spell.ToUpper().Replace(' ', '-'), "PLAYER", chat.getLine("player_1"), player_arr[player_ind].Stats.name);
             targetPattern = spellDict.getTargetPattern(s, enemy_arr, target_ind, player_arr, player_ind);
+        }
+        else if (status == CastStatus.BOTCH)
+        {
+            battleLog(spell.ToUpper().Replace(' ', '-'), "PLAYER", chat.getLine("botch"), player_arr[player_ind].Stats.name);
         }
         else
         {
@@ -264,7 +269,7 @@ public class BattleManager : MonoBehaviour {
         Pair<bool[], bool[]>  targetPattern = spellDict.getTargetPattern(s, player_arr, 1, enemy_arr, position);
         BattleEffects.main.setDim(true, enemy_arr[position].GetComponent<SpriteRenderer>());
         raiseTargets(targetPattern.second, targetPattern.first);
-        battleLog(s.ToString(), "ENEMY", "TODO: say something here", enemy_arr[position].Stats.name);
+        battleLog(s.ToString(), "ENEMY", chat.getLine("enemy_general_1"), enemy_arr[position].Stats.name);
 
         yield return new WaitForSeconds(1.5f);
 
