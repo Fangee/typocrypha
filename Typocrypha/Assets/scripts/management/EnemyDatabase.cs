1c9c18f6a1cf1aad1f0c7ccc817029c785dc0c65
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2018-02-02 20:48:35 -0800

    Fixed broken chat database from merge
    EnemyDatabase was missing chat database stuff from merge.

--- Typocrypha/Assets/scripts/management/EnemyDatabase.cs
+++ Typocrypha/Assets/scripts/management/EnemyDatabase.cs
@@ -72,6 +72,7 @@ public class EnemyDatabase
             string key = "";
             for (int j = ind + Elements.count; ; j++)
             {
+				Debug.Log (cols [j] + " " + j);
                 if (cols[j].Trim().CompareTo("END") == 0)
                 {
                     if (spells != null)
@@ -100,6 +101,7 @@ public class EnemyDatabase
                 if (style.CompareTo("null") == 0)
                     style = null;
                 SpellData s = new SpellData(root, elem, style);
+				Debug.Log (spells);
                 spells.Add(s);
             }
             EnemySpellList spellList = new EnemySpellList(spellGroups);
