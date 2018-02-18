7aa44776ff203f192fb641df52c2ed33e362aa5f
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2018-02-18 12:06:29 -0800

    Moved target brackets in front of enemy sprites

--- Typocrypha/Assets/scripts/Utility/PixelPerfectPosition.cs
+++ Typocrypha/Assets/scripts/Utility/PixelPerfectPosition.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 // adjust position of attached slightly for pixel perfection (aligned to pixels)
 public class PixelPerfectPosition : MonoBehaviour {
+	public bool update = false; // should position constantly be readjusted?
+
+	Camera main_camera; // main camera
+	float bound; // the size of one pixel in world coordinates
+
 	void Start () {
 		// get height and width of world (in world units)
-		Camera main_camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+		main_camera = GameObject.Find("Main Camera").GetComponent<Camera>();
 		float h = main_camera.orthographicSize * 2f;
-		float bound = h / Screen.height;
+		bound = h / Screen.height;
 		float x = transform.position.x - (transform.position.x % bound);
 		float y = transform.position.y - (transform.position.y % bound);
 		transform.position = new Vector2 (x, y);
-		Debug.Log (x + " " + y);
+	}
+
+	void LateUpdate() {
+		if (update) {
+			float x = transform.position.x - (transform.position.x % bound);
+			float y = transform.position.y - (transform.position.y % bound);
+			transform.position = new Vector2 (x, y);
+		}
 	}
 }
