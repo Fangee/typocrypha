9e393aa21b53a812ddf25e20ceca3003b82601fe
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2018-02-21 18:20:26 -0800

    Added Spacebar icon to dialogue box
    Spacebar lights up when prompting player to press space to advance text.

---
+++ Typocrypha/Assets/scripts/ui/DialogueBoxEffects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// manages effects on the dialogue box
+public class DialogueBoxEffects : MonoBehaviour {
+	public SpriteRenderer spacebar_icon; // icon for prompting spacebar
+	public TextScroll text_scroll; // text scroll script for the dialogue box
+	public Color gray; // graying out color for spacebar icon
+
+	void Update() {
+		if (text_scroll.is_print)
+			spacebar_icon.color = gray;
+		else
+			spacebar_icon.color = Color.white;
+	}
+}
