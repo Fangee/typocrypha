ee0fb9f8cd50b2563c6a2d7e3eafa305efcb1fca
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2018-02-23 19:09:37 -0800

    Made post processing effects more generic
    Now, post process has dual parallel arrays with properties and parameters

---
+++ Typocrypha/Assets/scripts/PostProcessing/PixelateScreen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// screen pixelation post processing effect
+public class PixelateScreen : PostProcess {
+	public float pixel_size;
+
+	void Start() {
+		shader_props [0] = "_PixelSize";
+	}
+
+	void Update() {
+		shader_params [0] = pixel_size;
+		for (int i = 0; i < shader_props.Length; ++i)
+			mat.SetFloat (shader_props[i], shader_params[i]);
+	}
+}
