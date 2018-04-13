d2213f996198ec83892abf37de449efa50636575
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2018-04-13 16:25:45 -0700

    Started Battle system
    Put some basic placeholders

---
+++ NewGameflowSystem/Assets/GameflowSystem/BattleSystem/Battle/Battle.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Represents a Battle with multiple rounds/cutscenes/etc
+[CreateAssetMenu]
+public class Battle : ScriptableObject {
+
+
+}
