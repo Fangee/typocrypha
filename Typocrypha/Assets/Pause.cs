40f39d89df5ba9aa2e9c4e5b2f57a3766e19c23d
Valentino Abate <vabate@UCSC.edu>
2018-05-19 14:07:09 -0700

    Finished merge, fixed pause bugs

--- Typocrypha/Assets/Pause.cs
+++ Typocrypha/Assets/Pause.cs
@@ -8,8 +8,8 @@ public class Pause : MonoBehaviour {
     Color dimCol;
     string pauseKey = "escape";
     GameObject hideChild;
-
-    bool battlePause; //saves battlemanager pause state
+    bool battlePause = false; //saves battlemanager pause state
+    bool blockTextboxInput = false;
 
     void Start () {
         gamePause = false;
@@ -29,6 +29,8 @@ public class Pause : MonoBehaviour {
                 BattleManagerS.main.setPause(true);
                 dimCol = BattleEffects.main.dimmer.color;
                 BattleEffects.main.setDim(true);
+                blockTextboxInput = DialogueManager.main.block_input;
+                DialogueManager.main.block_input = true;
                 AudioPlayer.main.pauseSFX();
 				hideChild.SetActive (true);
             }
@@ -37,6 +39,7 @@ public class Pause : MonoBehaviour {
                 Time.timeScale = 1;
                 BattleManagerS.main.setPause(battlePause);
                 BattleEffects.main.dimmer.color = dimCol;
+                DialogueManager.main.block_input = blockTextboxInput;
                 AudioPlayer.main.unpauseSFX();
 				hideChild.SetActive (false);
             }
