6443fc36547af0c196713959c1a1d2e8e5dbfcfb
Fangee <hughelv@gmail.com>
2018-06-21 21:48:35 -0700

    Set-up Pause Settings Sprites/Graphics
    - UI for the pause settings menu has been added; visual functionality is still unimplemented
    - The text scroll slider now has a tangible effect on the text scroll speed

--- Typocrypha/Assets/scripts/Settings/ScrollSlider.cs
+++ Typocrypha/Assets/scripts/Settings/ScrollSlider.cs
@@ -14,6 +14,6 @@ public class ScrollSlider : MonoBehaviour {
     }
 
     void ValueChangeCheck(){
-        DialogueManager.main.scroll_scale = slider.value;
+		DialogueManager.main.scroll_scale = 1+(11-(slider.value*10));
     }
 }
