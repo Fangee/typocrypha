5fcd9d37a441d62ea2f0e3d130f42ca7fd7b6924
Valentino Abate <vabate@UCSC.edu>
2018-07-03 14:03:05 -0700

    Mocked up some demo nodes and a demo canvas
    -Idea: macro that replace with text IF a certain flag is true or a certain value, add support for if-else constructs

---
+++ Node Editor Test/Assets/Custom_Node_Editor_Test/Core/Nodes/Event/AddCharacterNode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NodeEditorFramework;
+
+[Node(false, "Event/Add Character", new System.Type[] { typeof(GameflowCanvas) })]
+public class AddCharacterNode : GameflowStandardIONode {
+    public const string ID = "Add Character Node";
+    public override string GetID { get { return ID; } }
+    public string characterName;
+    public override Vector2 MinSize { get { return new Vector2(150, 40); } }
+    protected override void OnCreate()
+    {
+        characterName = "Character Name";
+    }
+
+    public override void NodeGUI()
+    {
+        characterName = GUILayout.TextField(characterName);
+    }
+}
