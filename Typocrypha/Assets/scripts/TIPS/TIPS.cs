aaa5f615a744158817421f96e3a439299bc5bbe2
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2018-07-07 14:39:13 -0700

    Laid down base for TIPS
    -Added placeholder UI.
    -TIPS is accessible by pressing TAB.
    -Added base opening/closing code.

---
+++ Typocrypha/Assets/scripts/TIPS/TIPS.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Top-level controller class for TIPS interface
+// Manages tab selection and changing
+public class TIPS : MonoBehaviour {
+    public GameObject TIPSUI; // Object containing UI for TIPS interface
+    public TIPSSearch tips_search; // Search tab controller
+
+    void Update()
+    {
+        // Toggle TIPS menu on 'TAB'
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            isOpen = !isOpen;
+        }
+    }
+
+    // Open/close TIPS menu
+    public bool isOpen 
+    {
+        // Get whether TIPS menu is open or not
+        get
+        {
+            return TIPSUI.activeInHierarchy;
+        }
+        // Open/close TIPS menu (true/false respectively)
+        set
+        {
+            Debug.Log("open TIPS:" + value);
+            Time.timeScale = 0;
+            TIPSUI.SetActive(value);
+            tips_search.isOpen = value; //TEMP: should open last opened tab
+        }
+    }
+	
+}
---
+++ Typocrypha/Assets/scripts/TIPS/TIPSSearch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Manages searching through TIPS database
+public class TIPSSearch : MonoBehaviour {
+    public InputField search_bar;
+
+    // Open/close TIPS search tab
+    public bool isOpen
+    {
+        // Get whether TIPS search tab is open or not
+        get
+        {
+            return gameObject.activeInHierarchy;
+        }
+        // Open/close TIPS search tab (true/false respectively)
+        set
+        {
+            Debug.Log("open TIPS search:" + value);
+            gameObject.SetActive(value);
+            if (value)
+            {
+                Debug.Log("activate");
+                search_bar.ActivateInputField();
+            }
+            else
+            {
+                Debug.Log("deactivate");
+                search_bar.DeactivateInputField();
+            }
+        }
+    }
+
+    // Called when text is submitted to search bar
+    public void submit()
+    {
+        Debug.Log("search:" + search_bar.text);
+    }
+}
