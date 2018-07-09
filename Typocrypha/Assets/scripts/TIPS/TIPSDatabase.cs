ce0452eed6dceea0a246c22ff6a0f4dc3ba7c5ef
James Takeshi Iwamasa <jiwamasa@ucsc.edu>
2018-07-09 10:50:23 -0700

    Added TIPS database and entry classes
    -Classes are essentially empty. Still waiting on full design implementation.

---
+++ Typocrypha/Assets/scripts/TIPS/TIPSDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Interfaces into TIPS database entries
+public class TIPSDatabase : MonoBehaviour {
+    public static TIPSDatabase main; // Global static reference
+
+    void Awake()
+    {
+        if (main == null)
+            main = this;
+        else
+            Destroy(this.gameObject);
+    }
+
+    // Queries database using a search term
+    public List<TIPSEntry> Query(string search)
+    {
+        return null;
+    }
+}
---
+++ Typocrypha/Assets/scripts/TIPS/TIPSEntry.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A single TIPS databse entry
+public class TIPSEntry : MonoBehaviour {
+    int id; // Unique id number
+    string title; // Name of entry
+}
