4a7509a9fbb08d5788920c37a180ae9fee1224c9
Valentino Abate <vabate@UCSC.edu>
2018-07-24 19:37:37 -0700

    Revamped character control node, removed individual character contriol nodes
    -Solved polymorphic serialization problem on editor side using scriptable objects and the AssetDatabase.AddObjectToAsset method (to condense everything into the same scriptable object - the gameflow node canvas)
    
    -Particulars of serialization piggyback of the node editor framework
    -Started click menus for clicking on node connections (currently middle-click, no functionality)
    -Generalized Reorderable list wrapper and put it in its own files (not 100% stablee yet TODO: add one with node connection support)

---
+++ Node Editor Test/Assets/Custom_Node_Editor_Test/Core/Nodes/Event/Character Control/CharacterControlNode.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using NodeEditorFramework;
+
+using NodeEditorFramework.Utilities;
+using System;
+
+namespace TypocryphaGameflow
+{
+    [Node(false, "Event/Character Control", new System.Type[] { typeof(GameflowCanvas) })]
+    public class CharacterControlNode : GameflowStandardIONode
+    {
+        public enum ControlType 
+        {
+            Add,
+            Remove,
+            Move,
+            Set_Expression,
+        }
+
+        public const string ID = "Character Control Node";
+        public override string GetID { get { return ID; } }
+
+        public override string Title { get { return "Character Control"; } }
+        public override Vector2 MinSize { get { return new Vector2(150, 20); } }
+
+        [SerializeField]
+        List<GUIUtilities.ReorderableListItem> _events;
+        public GUIUtilities.ReorderableItemList events = null;
+        protected override void OnCreate()
+        {
+            _events = new List<GUIUtilities.ReorderableListItem>();
+            _events.Add((GUIUtilities.ReorderableListItem)ScriptableObject.CreateInstance(typeof(AddCharacter)));
+        }
+
+        public override ScriptableObject[] GetScriptableObjects()
+        {
+            return _events.ToArray();
+        }
+
+        protected override void CopyScriptableObjects(Func<ScriptableObject, ScriptableObject> replaceSO)
+        {
+            for (int i = 0; i < _events.Count; ++i)
+                _events[i] = (GUIUtilities.ReorderableListItem)replaceSO(_events[i]);
+        }
+
+        public override void NodeGUI()
+        {
+            if (events == null)
+                events = new GUIUtilities.ReorderableItemList(_events, true, true, new GUIContent("Events", ""));
+            GUILayout.Space(5);
+            GUILayout.BeginVertical("box");
+            events.doLayoutList();
+            GUILayout.EndVertical();
+        }
+
+        public abstract class EventData : GUIUtilities.ReorderableListItem
+        {
+            public static IEnumerable<System.Type> subtypes = GUIUtilities.ReflectiveEnumerator.GetAllSubclassTypes<EventData>();
+            public string characterName = "name";
+            protected int numElements = 1;
+            public override void doAddMenu(IList list)
+            {
+                var menu = new NodeEditorFramework.Utilities.GenericMenu();
+                foreach (var type in subtypes)
+                {
+                    string[] path = type.ToString().Split('.');
+                    var name = path[path.Length - 1];
+                    menu.AddItem(new GUIContent(name),
+                    false, clickHandler, new GUIUtilities.MenuItemData(type, list));
+                }
+                menu.ShowAsContext();
+            }
+            private void clickHandler(object obj)
+            {
+                var data = (GUIUtilities.MenuItemData)obj;
+                data.list.Add(ScriptableObject.CreateInstance(data.type));
+            }
+            public override float getHeight(int index)
+            {
+                return base.getHeight(index) * 2;
+            }
+        }
+    }
+}
---
+++ Node Editor Test/Assets/Custom_Node_Editor_Test/Core/Nodes/Event/Character Control/Control Events/MoveCharacter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace TypocryphaGameflow
+{
+    public class MoveCharacter : CharacterControlNode.EventData
+    {
+        public enum MovementType
+        {
+            Set_Position,
+            Fade_In,
+            Fade_Out,
+            Crossfade,
+            Slide,
+        }
+        public MovementType moveType = MovementType.Set_Position;
+        public Vector2 targetPos = new Vector2(0, 0);
+        public float time = 1.0f;
+        private int addLines = 0;
+
+        public override void doGUI(Rect rect, int index, IList list)
+        {
+            Rect UIrect = new Rect(rect);
+            UIrect.height = EditorGUIUtility.singleLineHeight;
+            GUI.Label(UIrect, new GUIContent("Move Character", ""), new GUIStyle(GUIStyle.none) { alignment = TextAnchor.MiddleCenter });
+            UIrect.y += EditorGUIUtility.singleLineHeight + 1;
+            characterName = GUI.TextField(UIrect, characterName);
+            UIrect.y += EditorGUIUtility.singleLineHeight + 1;
+            moveType = (MovementType)EditorGUI.EnumPopup(UIrect, moveType);
+            UIrect.y += EditorGUIUtility.singleLineHeight + 1;
+            if (moveType != MovementType.Set_Position)
+            {
+                time = EditorGUI.FloatField(UIrect, time);
+                UIrect.y += EditorGUIUtility.singleLineHeight + 1;
+                addLines = 1;
+            }
+            else
+                addLines = 0;
+            targetPos = EditorGUI.Vector2Field(UIrect, "", targetPos);
+        }
+
+        public override float getHeight(int index)
+        {
+            return EditorGUIUtility.singleLineHeight * (4 + addLines) + (4 + addLines);
+        }
+    }
+
+}
