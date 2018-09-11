6b215eeca3d2ccceca5a1c3a0acf74c42465e0a4
Hana Cho <hanacho252@gmail.com>
2018-09-10 18:12:49 -0700

    started new title sequence: set up files and text boxes

---
+++ Typocrypha/Assets/scripts/TitleScreenNew/KeyboardTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardTest : MonoBehaviour 
+{
+	// References to text box objects
+	public Text version;
+	public Text team;
+	public Text instructions;
+	public Text testPhrase;
+	public InputField inputField;
+	
+	// Reference to necessary scripts
+	public TitleScreenNew titleScreen;
+	public TextScroll textScroll;
+	
+	// Text to be placed in scrolling text boxes
+	public string instructionText;
+	public string testPhraseText;
+	
+	void Start() 
+	{
+		// Fade in
+		
+		StartCoroutine(scrollText());
+		
+		// when done, activate text field (put into focus)
+	}
+	
+	void Update() 
+	{
+		// if key pressed, play sound
+		// onSubmit():
+		// if correct text, go to title screen, play sound
+		// fade out of keyboard test, fade into title screen
+		// if incorrect text, clear text, play sound
+	}
+	
+	// Scrolls the two lines of text
+	IEnumerator scrollText()
+	{
+		textScroll.startPrint(instructionText, instructions);
+		yield return new WaitUntil(() => !textScroll.is_print); // wait for text to finish scrolling
+		
+		textScroll.startPrint(testPhraseText, testPhrase);
+		yield return new WaitUntil(() => !textScroll.is_print);
+	}
+}
---
+++ Typocrypha/Assets/scripts/TitleScreenNew/TitleScreenNew.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreenNew : MonoBehaviour 
+{
+	// public List<MenuItem> menuItems = new List<MenuItem>();
+	// MenuItem Class:
+	// text box property
+	// or maybe string property and just create the text box in script might be less painful
+	// OnClick()
+	
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+}
